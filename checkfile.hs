56f9f8a816dd98e925bf07439ddb5d64029803ed
Neil Shepperd <nshepperd@gmail.com>
2013-09-20 15:58:50 +1000

    Delete some dead code and add a few comments

--- checkfile.hs
+++ checkfile.hs
@@ -7,44 +7,47 @@ import Data.Word (Word32)
 import Text.Printf (printf)
 import Text.Regex.Posix ((=~))
 
-
-main :: IO ()
-main = getArgs >>= (mapM_ checkcrc)
-
--- re_crc = mkRegex "^.*\\[([0-9A-F]{8})\\].*$"
-
-checkcrc :: String -> IO ()
-checkcrc fname = do
-  case extractcrc fname of
-    Just (prefix, suffix, goal) -> do
-      crc <- fmap wtox (getcrc fname)
-      if crc == goal then
-          putStrLn (green crc ++ "   " ++ prefix ++ "[" ++ green goal ++ "]" ++ suffix)
-      else
-          putStrLn (red crc ++ "   " ++ prefix ++ "[" ++ red goal ++ "]" ++ suffix)
-    Nothing -> return ()
-
+-- Text coloring
 green :: String -> String
 green str = "\x1b[01;32m" ++ str ++ "\x1b[m"
 red :: String -> String
 red str = "\x1b[01;31m" ++ str ++ "\x1b[m"
 
--- printcheck :: String -> String -> IO ()
--- printcheck goal crc = do
+-- Integer to hexadecimal
+wtox :: Word32 -> String
+wtox x = printf "%.8X" x
 
+-- Calculate CRC from file contents
+-- (may throw IOException)
+contentscrc :: String -> IO Word32
+contentscrc fname = do
+  file <- openBinaryFile fname ReadMode
+  dat <- L.hGetContents file
+  -- The file will be once the CRC is calculated
+  -- causing hGetContents to encounter EOF
+  return (crc32 dat)
+
+-- Try to extract the required CRC from the filename
 extractcrc :: String -> Maybe (String, String, String)
 extractcrc fname = if not (null matchlist) then
                        Just (prefix, suffix, head matchlist)
                    else
                        Nothing
-    where (prefix, _, suffix, matchlist) = fname =~ "\\[([0-9A-F]{8})\\]"
+    where (prefix, _, suffix, matchlist) = (fname =~ "\\[([0-9A-F]{8})\\]")
                                :: (String, String, String, [String])
 
-getcrc :: String -> IO Word32
-getcrc fname = do
-  handle <- openBinaryFile fname ReadMode
-  dat <- L.hGetContents handle
-  return (crc32 dat)
+-- Check CRC given in filename against file contents
+-- Ignores filenames that don't have a checksum in [SQUARE BRACKETS]
+checkcrc :: String -> IO ()
+checkcrc fname = do
+  case extractcrc fname of
+    Just (prefix, suffix, goal) -> do
+      checksum <- fmap wtox (contentscrc fname)
+      if checksum == goal then
+        putStrLn (green checksum ++ "   " ++ prefix ++ "[" ++ green goal ++ "]" ++ suffix)
+      else
+        putStrLn (red checksum ++ "   " ++ prefix ++ "[" ++ red goal ++ "]" ++ suffix)
+    Nothing -> return ()
 
-wtox :: Word32 -> String
-wtox x = printf "%.8X" x
+main :: IO ()
+main = getArgs >>= (mapM_ checkcrc)
